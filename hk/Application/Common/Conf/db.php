0d0088a8e0a928299ef493472f8c7d3d91709464
wangxuan <865387432@qq.com>
2015-04-18 13:02:03 +0800

    hackthon

---
+++ hk/Application/Common/Conf/db.php
@@ -0,0 +1,10 @@
+<?php 
+ return array( 
+  'DB_TYPE' => 'mysqli',
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'hk',
+  'DB_USER' => 'root',
+  'DB_PWD' => 'xuan',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'hk_',
+);
